f32b54486ed44fbe96527cecca3bf716b6bec6fe
Oleg Klimenko <oklimenko@gmail.com>
2022-10-08 20:39:07 -0700

    Remove element

--- src/main/java/com/oklimenko/algo/list/ReverseLinkedList.java
+++ src/main/java/com/oklimenko/algo/list/ReverseLinkedList.java
@@ -1,10 +1,14 @@
 package com.oklimenko.algo.list;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReverseLinkedList {
 
     public static class ListNode {
         int val;
         ListNode next;
+
         ListNode(int x) {
             val = x;
             next = null;
@@ -13,8 +17,12 @@ public class ReverseLinkedList {
 
     public static void main(String[] args) {
 
+        int arr[] = {1, 2, 6, 3, 4, 5, 6};
+        int val = 6;
+
+
 //        int arr[] = {1, 2, 3, 4, 5};
-        int arr[] = {};
+//        int arr[] = {};
 //    int []listB = {5,6,1,8,4,5};
 //    int intersectVal = 8;
         ListNode headA = null;
@@ -22,16 +30,40 @@ public class ReverseLinkedList {
             headA = addAtTail(headA, arr[i]);
         }
 
-        ListNode newHead = reverseList(headA);
+        ListNode newHead = removeElements(headA, val);
+//        ListNode newHead = reverseList(headA);
         while (newHead != null) {
             System.out.println(newHead.val);
             newHead = newHead.next;
         }
     }
 
+    public static ListNode removeElements(ListNode head, int val) {
+        ListNode newHead = new ListNode(0);
+        newHead.next = head;
+        ListNode curr = newHead;
+        while (curr != null && curr.next != null) {
+            if (curr.next.val == val) {
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+            }
+        }
+        return newHead.next;
+    }
+
+    public static List<Integer> listToArr(ListNode head) {
+        ArrayList<Integer> list = new ArrayList<>();
+        while (head!=null) {
+            list.add(head.val);
+            head = head.next;
+        }
+        return list;
+    }
+
     public static ListNode reverseList(ListNode head) {
         ListNode newHead = head;
-        while (head!=null && head.next!=null) {
+        while (head != null && head.next != null) {
             ListNode itemToMove = head.next;
             head.next = itemToMove.next;
             itemToMove.next = newHead;
