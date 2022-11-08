74d0089982b5231ddcc21246023bee3587aa5003
Oleg_Klimenko <oleg_klimenko@epam.com>
2022-11-07 19:56:52 -0800

    add two digits

--- src/main/java/com/oklimenko/algo/list/MyLinkedList.java
+++ src/main/java/com/oklimenko/algo/list/MyLinkedList.java
@@ -7,13 +7,38 @@ public class MyLinkedList {
 
     public static void main(String[] args) {
 
+//        int l1[] = {0};//{2,4,3};
+//        int l2[] = {0};//{5,6,4};
+
+        int l1[] = {9,9,9,9,9,9,9};
+        int l2[] = {9,9,9,9};
+//        [8,9,9,9,0,0,0,1]
+//        Output: [7,0,8]
+        MyLinkedList myLinkedList1 = new MyLinkedList();
+        for (int i = 0; i < l1.length; i++) {
+            myLinkedList1.addAtTail(l1[i]);
+        }
+
+        MyLinkedList myLinkedList2 = new MyLinkedList();
+        for (int i = 0; i < l2.length; i++) {
+            myLinkedList2.addAtTail(l2[i]);
+        }
+
+        ListNode result = addTwoNumbers(myLinkedList1.head, myLinkedList2.head);
+        ListNode curr = result;
+        while (curr != null) {
+            System.out.print(curr.val + ",");
+            curr = curr.next;
+        }
+        System.out.println();
+
 //        [null,null,null,null,-1,2,null,null,null,null,null,null,null,null,84,null,null,null,null,null,null,null,null,null,null,null,16,null,null,null,null,null,null,null,null,37,null,null,null,null,null,23,null,null,null,null,null,null,null,null,null,null,null,null,null,null,19,null,17,null,null,56,null,null,31,null,17,null,12,null,null,null,null,null,null,null,40,null,null,null,37,null,76,null,-1,null,null,null,null,null,null,80,null,null,null,null,43,null,null,null,-1,null]
 //        [null,null,null,null,-1,2,null,null,null,null,null,null,null,null,84,null,null,null,null,null,null,null,null,null,null,null,16,null,null,null,null,null,null,null,null,37,null,null,null,null,null,23,null,null,null,null,null,null,null,null,null,null,null,null,null,null,19,null,17,null,null,56,null,null,31,null,17,null,12,null,null,null,null,null,null,null,40,null,null,null,37,null,76,null,42,null,null,null,null,null,null,80,null,null,null,null,43,null,null,null,40,null]
 
 //    ["MyLinkedList","addAtHead","addAtTail","addAtTail","get","get","addAtTail","addAtIndex","addAtHead","addAtHead","addAtTail","addAtTail","addAtTail","addAtTail","get","addAtHead","addAtHead","addAtIndex","addAtIndex","addAtHead","addAtTail","deleteAtIndex","addAtHead","addAtHead","addAtIndex","addAtTail","get","addAtIndex","addAtTail","addAtHead","addAtHead","addAtIndex","addAtTail","addAtHead","addAtHead","get","deleteAtIndex","addAtTail","addAtTail","addAtHead","addAtTail","get","deleteAtIndex","addAtTail","addAtHead","addAtTail","deleteAtIndex","addAtTail","deleteAtIndex","addAtIndex","deleteAtIndex","addAtTail","addAtHead","addAtIndex","addAtHead","addAtHead","get","addAtHead","get","addAtHead","deleteAtIndex","get","addAtHead","addAtTail","get","addAtHead","get","addAtTail","get","addAtTail","addAtHead","addAtIndex","addAtIndex","addAtHead","addAtHead","deleteAtIndex","get","addAtHead","addAtIndex","addAtTail","get","addAtIndex","get","addAtIndex","get","addAtIndex","addAtIndex","addAtHead","addAtHead","addAtTail","addAtIndex","get","addAtHead","addAtTail","addAtTail","addAtHead","get","addAtTail","addAtHead","addAtTail","get","addAtIndex"]
 //    [[],[84],[2],[39],[3],[1],[42],[1,80],[14],[1],[53],[98],[19],[12],[2],[16],[33],[4,17],[6,8],[37],[43],[11],[80],[31],[13,23],[17],[4],[10,0],[21],[73],[22],[24,37],[14],[97],[8],[6],[17],[50],[28],[76],[79],[18],[30],[5],[9],[83],[3],[40],[26],[20,90],[30],[40],[56],[15,23],[51],[21],[26],[83],[30],[12],[8],[4],[20],[45],[10],[56],[18],[33],[2],[70],[57],[31,24],[16,92],[40],[23],[26],[1],[92],[3,78],[42],[18],[39,9],[13],[33,17],[51],[18,95],[18,33],[80],[21],[7],[17,46],[33],[60],[26],[4],[9],[45],[38],[95],[78],[54],[42,86]]
 
-        MyLinkedList myLinkedList = new MyLinkedList();
+       /* MyLinkedList myLinkedList = new MyLinkedList();
         myLinkedList.addAtHead(84);
         myLinkedList.addAtTail(2);
         myLinkedList.addAtTail(39);
@@ -114,7 +139,7 @@ public class MyLinkedList {
         myLinkedList.addAtHead(95);
         myLinkedList.addAtTail(78);
         myLinkedList.get(54);
-        myLinkedList.addAtIndex(42,86);
+        myLinkedList.addAtIndex(42,86);*/
 
 
        /* String s1 = "[\"MyLinkedList\",\"addAtHead\",\"addAtTail\",\"addAtTail\",\"get\",\"get\",\"addAtTail\",\"addAtIndex\",\"addAtHead\",\"addAtHead\",\"addAtTail\",\"addAtTail\",\"addAtTail\",\"addAtTail\",\"get\",\"addAtHead\",\"addAtHead\",\"addAtIndex\",\"addAtIndex\",\"addAtHead\",\"addAtTail\",\"deleteAtIndex\",\"addAtHead\",\"addAtHead\",\"addAtIndex\",\"addAtTail\",\"get\",\"addAtIndex\",\"addAtTail\",\"addAtHead\",\"addAtHead\",\"addAtIndex\",\"addAtTail\",\"addAtHead\",\"addAtHead\",\"get\",\"deleteAtIndex\",\"addAtTail\",\"addAtTail\",\"addAtHead\",\"addAtTail\",\"get\",\"deleteAtIndex\",\"addAtTail\",\"addAtHead\",\"addAtTail\",\"deleteAtIndex\",\"addAtTail\",\"deleteAtIndex\",\"addAtIndex\",\"deleteAtIndex\",\"addAtTail\",\"addAtHead\",\"addAtIndex\",\"addAtHead\",\"addAtHead\",\"get\",\"addAtHead\",\"get\",\"addAtHead\",\"deleteAtIndex\",\"get\",\"addAtHead\",\"addAtTail\",\"get\",\"addAtHead\",\"get\",\"addAtTail\",\"get\",\"addAtTail\",\"addAtHead\",\"addAtIndex\",\"addAtIndex\",\"addAtHead\",\"addAtHead\",\"deleteAtIndex\",\"get\",\"addAtHead\",\"addAtIndex\",\"addAtTail\",\"get\",\"addAtIndex\",\"get\",\"addAtIndex\",\"get\",\"addAtIndex\",\"addAtIndex\",\"addAtHead\",\"addAtHead\",\"addAtTail\",\"addAtIndex\",\"get\",\"addAtHead\",\"addAtTail\",\"addAtTail\",\"addAtHead\",\"get\",\"addAtTail\",\"addAtHead\",\"addAtTail\",\"get\",\"addAtIndex\"]";
@@ -239,6 +264,38 @@ public class MyLinkedList {
 //[[],[],[],[],[],[],[],[],[4],[4],[5,0],[6]]
     }
 
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//        9,9,9,9,9,9,9
+//        9,9,9,9
+        ListNode result = new ListNode(0);
+        ListNode headRes = result;
+        int nextVal = 0;
+        while (l1!=null || l2!=null || nextVal!=0) {
+            int sum = nextVal;
+            int val1;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+//            int sum = val1 + val2 + nextVal;
+            int val = sum % 10;
+            nextVal = sum / 10;
+            ListNode nextResult = new ListNode(val);
+            result.next = nextResult;
+            result = nextResult;
+        }
+//        if ( nextVal!=0 ) {
+//            ListNode nextResult = new ListNode(nextVal);
+//            result.next = nextResult;
+//            result = nextResult;
+//        }
+        return headRes.next;
+    }
+
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
         ListNode newHead = new ListNode(0);
@@ -275,7 +332,7 @@ public class MyLinkedList {
 
     boolean isPrint = false;
 
-    class ListNode {
+    static class ListNode {
         int val;
         ListNode next, prev;
 
